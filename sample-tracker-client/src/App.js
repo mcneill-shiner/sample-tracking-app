816ca73e4b4cfe4fcc27cdc0d376a441eca7f20b
McNeill Shiner <mcneill.shiner@gmail.com>
2023-10-24 21:06:13 -0400

    set up main component

--- sample-tracker-client/src/App.js
+++ sample-tracker-client/src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+// additional imports
 
 function App() {
   return (
-
+    // wrap the AppContent component with AuthProvider to provide auth context
+    <AuthProvider>
+      <AppContent /> {/* this is where main app content will be rendered */}
+    </AuthProvider>
   );
 }
 
