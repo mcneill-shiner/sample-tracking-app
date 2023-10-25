dca7d094df0b05717dd0af419510f077c1842353
McNeill Shiner <mcneill.shiner@gmail.com>
2023-10-24 21:53:43 -0400

    created ProjectList component

---
+++ sample-tracker-client/src/components/ProjectList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProjectList = {{projects, openProject}} => {
+    return (
+        <div>
+            <h2>
+                Projects
+            </h2>
+            <ul>
+                {rooms.map((room, index) => (
+                    <li key={index}>
+                        <button onClick={() => openProject(project)}>Open {room}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
