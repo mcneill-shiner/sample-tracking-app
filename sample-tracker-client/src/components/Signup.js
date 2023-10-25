48ca419069d83308148303b3575b46617347d08a
McNeill Shiner <mcneill.shiner@gmail.com>
2023-10-25 18:42:05 -0400

    Set up Signup.js

---
+++ sample-tracker-client/src/components/Signup.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const SignupForm = ({onSignup}) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:8000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, email, password}),
+        });
+
+        const data = await response.json();
+        
+        onSignup(data.token, data.username)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type='email'
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+                type='password'
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type='submit'>Sign Up</button>
+        </form>
+    )
+}
+
+export default SignupForm;
